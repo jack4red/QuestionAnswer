d92d3012ff7652c8ca514d618670766ef207ef27
Chandan <chandan.yeshwanth@gmail.com>
2015-03-18 17:06:54 +0530

    removed "answers" view from QuestionAnswer
    answers will be shown in "detail" view along with the question

--- QuestionAnswer/urls.py
+++ QuestionAnswer/urls.py
@@ -7,8 +7,6 @@ urlpatterns = patterns('',
 	url(r'^$', views.index, name='index'),
 	# /QuestionAnswer/1
 	url(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
-	# /QuestionAnswer/1/answers
-	url(r'^(?P<question_id>\d+)/answers/$', views.answers, name='answers'),
 	# /QuestionAnswer/1/write_answer
 	url(r'^(?P<question_id>\d+)/write_answer/$', views.write_answer, name='write_answer'),
 )
