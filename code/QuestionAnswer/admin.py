9967c924cae679c004587c942b2b7a2803dc86b1
topPC_zk <1120429511@qq.com>
2016-04-30 21:02:21 +0800

    change readme

--- code/QuestionAnswer/admin.py
+++ code/QuestionAnswer/admin.py
@@ -3,16 +3,16 @@ from QuestionAnswer.models import Question, Answer
 
 # Register your models here.
 
-class AnswerInline(admin.TabularInline):
-	model = Answer
-	extra = 3
+# class AnswerInline(admin.TabularInline):
+# 	model = Answer
+# 	extra = 3
 
-class QuestionAdmin(admin.ModelAdmin):
-	fields = ['question_text']
-	inlines = [AnswerInline]
+# class QuestionAdmin(admin.ModelAdmin):
+# 	fields = ['question_text']
+# 	inlines = [AnswerInline]
 	
-	list_display = ('question_text', 'pub_date')
-	list_filter = ['pub_date']
-	search_fields = ['question_text']
+# 	list_display = ('question_text', 'pub_date')
+# 	list_filter = ['pub_date']
+# 	search_fields = ['question_text']
 
-admin.site.register(Question, QuestionAdmin)
+# admin.site.register(Question, QuestionAdmin)
